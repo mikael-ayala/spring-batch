5c4095d293228aa6378400138cd70bf55d076b87
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-08-18 15:22:10 -0300

    initial commit, add Job and Step configuration

---
+++ src/main/java/com/devsuperior/hello_world_spring_batch/HelloWorldSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hello_world_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/hello_world_spring_batch/HelloWorldSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hello_world_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
